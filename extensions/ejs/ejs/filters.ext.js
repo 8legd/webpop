534fbbca7e9f8733d1220ebb58033d5c4f1089a3
8legd <git@8legd.com>
2012-06-21 23:51:29 +0100

    doc

--- extensions/ejs/ejs/filters.ext.js
+++ extensions/ejs/ejs/filters.ext.js
@@ -1,6 +1,9 @@
+/*!
+ * Add any extra filters for your views here... 
+ */
 exports.addTo = function(ejs) {
   
-  // filter to convert richtext to plain text by converting / removing html markup
+  // an example filter to convert rich text to plain text by converting / removing html markup
   ejs.filters.plaintext = function(str) {
     var result = "";
     // replace any <br/>'s
