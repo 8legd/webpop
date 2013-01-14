49d0a7ae0be8912e2d55fdc8fa41296118e6db4a
8legd <git@8legd.com>
2013-01-14 22:51:12 +0000

    tidy up

--- extensions/app.js
+++ extensions/app.js
@@ -3,7 +3,11 @@
  */
 webpop = true;
 
-CONFIG = require('config').options;
+CONFIG = require('config');
+// This is the production config file - not added to GitHub
+// Create your own config.js in the WebPop extensions directory
+// For a list of the expected values see the config.js in the root
+CONFIG = CONFIG && CONFIG.options;
 
 // nodify our WebPop app
 require('nodify');
